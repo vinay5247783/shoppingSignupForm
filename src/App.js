0d148a6352329d5b72b2489eb90de7eefded02c8
vinay <vverma@dminc.com>
2020-10-22 11:27:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import Register from './register.js';
 import './App.css';
+import Particles from 'react-particles-js';
+import Logo from './Logo.js';
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles className='particles'
+          params={particlesOptions}
+        />
+      <Logo/>
+
+      <Register/>
     </div>
   );
 }
 
 export default App;
+
+
